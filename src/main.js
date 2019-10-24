8691ac8c879468b1f86f828e3abb2539cc7c3f4f
Diogo Marques <diogo@fontainhas.com>
2019-10-24 21:47:05 +0100

    Hello world

---
+++ src/main.js
@@ -0,0 +1,30 @@
+import * as THREE from 'three';
+
+
+const WIDTH = window.innerWidth;
+const HEIGHT = window.innerHeight;
+
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera( 75, WIDTH / HEIGHT, 0.1, 1000 );
+
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize( WIDTH, HEIGHT );
+document.body.appendChild( renderer.domElement );
+
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+    requestAnimationFrame( animate );
+    
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+}
+animate();
