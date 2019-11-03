3807d00881492c82fae6423bd4b14d62f619dfc2
Diogo Marques <diogo@fontainhas.com>
2019-11-03 22:31:55 +0000

    github Actions test

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import { TweenMax, Expo } from "gsap/all"; 
-import { Utils } from "../Utils/Utils";
+import { Utils } from "../Utils/Utils.js";
 import { Block } from "./components/Block";
 import { GameUi } from "./components/GameUi";
 
